4d5ee36661e406c17ff30cd2c2195d250bed7187
Khanh Trinh (Spite) <118985514+ks1610@users.noreply.github.com>
2024-12-16 11:05:25 +0700

    Update main.cpp

--- CTDL/Tree/main.cpp
+++ CTDL/Tree/main.cpp
@@ -22,6 +22,56 @@ struct SVTree
 	Node * root; // Con tro toi nut goc cua cay
 };
 
+typedef int T;
+
+struct QueueNode
+{
+	Node * sv;
+	QueueNode * next;
+	QueueNode(Node * n): sv(n), next(NULL){}
+};
+
+struct Queue
+{
+	int size;
+	QueueNode * Front;
+	QueueNode * Back;
+	Queue(): size(0), Front(NULL), Back(NULL){} 
+
+	void enqueue(Queue & q, Node * e){
+		QueueNode * v = new QueueNode(e);
+
+		if(q.size == 0)
+			q.Front = q.Back = v;
+		else{
+			q.Back->next = v;
+			q.Back = v;
+		}
+		q.size++;
+	}
+
+	Node* dequeue(Queue & q){
+    	if (q.size == 0) {
+    	    throw std::runtime_error("Queue is empty.");
+    	}
+
+    	Node* e = q.Front->sv;
+		QueueNode * v = q.Front;
+
+		if(q.size == 1)
+			q.Front = q.Back = NULL;
+		else 
+			q.Front = q.Front->next;
+
+		delete v;
+		q.size--;
+
+		return e;
+	}
+};
+
+
+
 // Ham khoi tao cay (ban dau cay rong)
 void treeInit(SVTree & tree);
 
@@ -49,31 +99,42 @@ Node * treeSearch(SVTree & tree, int maSV);
 // Tim sinh vien theo ma SV (t la goc cua cay)
 Node * treeSearch(int maSV, Node * t);
 
-void treePostOrderTraveral(Node *& t)
-{
-    if (t == NULL)
-        return;
-    
-    // Traverse left subtree
-    treePostOrderTraveral(t->left);
-    
-    // Traverse right subtree
-    treePostOrderTraveral(t->right);
-    
-    // Print current node's maSV
-    std::cout << t->maSV << " " << t->hoTen<<" -> ";
-}
+// YEU CAU THUC HANH
+//   1. Khai bao cac ham duyet cay theo thu tu truoc, giua va sau
+//   2. Khai bao cac ham dem so nut, so nut la va so nut co du 2 con
+//   3. Khai bao ham duyet cay theo thu tu muc (xem bai tap 7 o cuoi bai giang cay va cay nhi phan)
+//   4. Khai bao ham in ra cac gia tri tren cay nam trong doan [k1; k2]
+
+// VIET CODE CUA BAN O DAY
+// ...
 
 void treePreOrderTraveral(Node *& t)
 {
     if (t == NULL)
         return;
     
+    // Print current node's maSV
+    std::cout << t->maSV << " " << t->hoTen<<" -> ";
+	
     // Traverse left subtree
-    treePostOrderTraveral(t->right);
-    
+    treePreOrderTraveral(t->left);
+
     // Traverse right subtree
+    treePreOrderTraveral(t->right);
+
+    
+}
+
+void treePostOrderTraveral(Node *& t)
+{
+    if (t == NULL)
+        return;
+    
+	// Traverse left subtree
     treePostOrderTraveral(t->left);
+
+    // Traverse right subtree
+    treePostOrderTraveral(t->right);
     
     // Print current node's maSV
     std::cout << t->maSV << " " << t->hoTen<<" -> ";
@@ -84,19 +145,15 @@ void treeInOrderTraveral(Node *& t)
         return;
     
     // Traverse left subtree
-    treePostOrderTraveral(t->left);
+    treeInOrderTraveral(t->left);
 
     // Print current node's maSV
     std::cout << t->maSV << " " << t->hoTen<<" -> ";
     
     // Traverse right subtree
-    treePostOrderTraveral(t->right);
-    
+    treeInOrderTraveral(t->right);
 }
-/*void PostOrderTraveral(SVTree & tree)
-{
-	treePostOrderTraveral(tree.root);
-}*/
+
 
 int treeCountNode(Node *& t)
 {
@@ -121,14 +178,45 @@ int treeCountFullNode(Node *& t)
 	int isFull = (t->left != NULL && t->right != NULL) ? 1 : 0;
 	return isFull + treeCountFullNode(t->left) + treeCountFullNode(t->right);
 }
-// YEU CAU THUC HANH
-//   1. Khai bao cac ham duyet cay theo thu tu truoc, giua va sau
-//   2. Khai bao cac ham dem so nut, so nut la va so nut co du 2 con
-//   3. Khai bao ham duyet cay theo thu tu muc (xem bai tap 7 o cuoi bai giang cay va cay nhi phan)
-//   4. Khai bao ham in ra cac gia tri tren cay nam trong doan [k1; k2]
 
-// VIET CODE CUA BAN O DAY
-// ...
+void treelevelOrderTraversal(Node* tree)
+{
+	if(tree == NULL)
+		return;
+	
+	Queue q;
+	q.enqueue(q, tree);
+
+	while(q.size != 0){
+		Node* node = q.dequeue(q);
+
+        // Print the current node's value
+        cout << node->maSV << " " << node->hoTen << " -> ";
+
+		if(node->left != NULL)
+			q.enqueue(q, node->left);
+		
+		if(node->right != NULL)
+			q.enqueue(q, node->right);
+	}
+
+}
+void treePrintRange(Node * tree, int k1, int k2)
+{
+	if(tree == NULL)
+		return;
+
+	if(tree->maSV > k1)
+		treePrintRange(tree->left, k1, k2);
+
+	if(tree->maSV >= k1 && tree->maSV <= k2)
+    	std::cout << tree->maSV << " " << tree->hoTen << " -> ";
+	
+	if(tree->maSV < k2)
+		treePrintRange(tree->right, k1, k2);
+}
+
+
 
 int main()
 {
@@ -146,34 +234,39 @@ int main()
 	treeInsert(tree, 3, "Tien Porn");
 	treeInsert(tree, 9, "Tien Xem Porn");
 	/*
-						  4
-						/  \
-					   2	5
-					  /		 \
-					3		  8			
-							 / \
-							6   9   			
+		  4
+        /   \
+       2     5
+        \     \
+         3     8
+              / \
+             6   9
 	*/
 	
 	//   2. Viet code tim sinh vien theo ma sinh vien:
 	//       - Neu tim thay, in ho ten sinh vien len man hinh
 	//       - Neu khong tim thay, thong bao khong tim duoc
+	std::cout<<std::endl;
 	Node * found = treeSearch(tree, 6);
-	std::cout<<(found ? found->hoTen : "Khong Tim Duoc")<<std::endl;
+	std::cout<<(found ? "Sinh Vien: " + found->hoTen : "Khong Tim Duoc")<<std::endl;
 	
 	//   3. Viet code duyet cay theo thu tu truoc, giua va sau
+	std::cout<<std::endl;
 	std::cout<<"Duyet Theo Thu Tu Truoc:"<<std::endl;
+	treePreOrderTraveral(tree.root);
+	
 	//PostOrderTraveral(tree);
-	treePostOrderTraveral(tree.root);
 	std::cout<<std::endl;
 	std::cout<<"Duyet Theo Thu Tu Sau:"<<std::endl;
-	treePreOrderTraveral(tree.root);
+	treePostOrderTraveral(tree.root);
+	
 	std::cout<<std::endl;
 	std::cout<<"Duyet Tu Giua:"<<std::endl;
 	treeInOrderTraveral(tree.root);
 	std::cout<<std::endl;
 
 	//   4. Viet code dem so nut, so nut la va so nut co du 2 con
+	std::cout<<std::endl;
 	std::cout<<"So Nut Con: "<<treeCountNode(tree.root)<<std::endl;
 	std::cout<<"So Nut La Con: "<<treeCountLeafNode(tree.root)<<std::endl;
 	std::cout<<"So Nut Con Day Du: "<<treeCountFullNode(tree.root)<<std::endl;
@@ -181,13 +274,26 @@ int main()
 	std::cout<<std::endl;
 
     //   5. Viet code duyet cay theo thu thu muc
+	std::cout<<std::endl;
+	treelevelOrderTraversal(tree.root);
+	std::cout<<std::endl;
 
     //   6. Viet code in ra cac gia tri tren cay nam trong doan [k1; k2]
-    
+	std::cout<<std::endl;
+    treePrintRange(tree.root, 2, 6);
+	std::cout<<std::endl;
+
 	//   7. Viet code xoa rong cay
-	
+	std::cout<<std::endl;
+	std::cout<<"Xoa Cay...";
+	treeMakeEmpty(tree);
+	std::cout<<std::endl;
+
 	//   8. Viet code kiem tra xem cay da rong hay chua
-	
+	std::cout<<std::endl;
+	std::cout<<(treeIsEmpty(tree)? "Cay Rong" : "Cay Chua Rong");
+	std::cout<<std::endl;
+
 	// VIET CODE CUA BAN O DAY
 	// ...
 	
